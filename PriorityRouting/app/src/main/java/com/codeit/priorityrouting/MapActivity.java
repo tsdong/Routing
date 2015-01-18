929adae0f26efff0a5c87a04877230deea37cd62
James Smith <jnsmith@oakland.edu>
2015-01-18 17:33:17 -0500

    Iteration 1
    Implemented "Splash Screen", added "Address Entry", and added the "Maps"
    page.

--- PriorityRouting/app/src/main/java/com/codeit/priorityrouting/MapActivity.java
+++ PriorityRouting/app/src/main/java/com/codeit/priorityrouting/MapActivity.java
@@ -1,40 +1,24 @@
 package com.codeit.priorityrouting;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import android.os.Handler;
 
-
-public class SplashActivity extends Activity {
-
-    private static int SPLASH_TIME_OUT = 5000;
+public class MapActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this,AddressActivity.class);
-                startActivity(i);
-
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        setContentView(R.layout.activity_map);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
