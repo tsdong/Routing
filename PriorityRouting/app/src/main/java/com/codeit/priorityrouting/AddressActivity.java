d2d6f84df5971b9cc8c181f1bee696ba6a4ce64c
James Smith <jnsmith@oakland.edu>
2015-01-19 23:23:10 -0500

    Iteration 2
    Implemented Google Maps.

--- PriorityRouting/app/src/main/java/com/codeit/priorityrouting/AddressActivity.java
+++ PriorityRouting/app/src/main/java/com/codeit/priorityrouting/AddressActivity.java
@@ -1,9 +1,12 @@
 package com.codeit.priorityrouting;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class AddressActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class AddressActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
+
+        final Button mapButton = (Button) findViewById(R.id.startMap);
+        mapButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent i = new Intent(AddressActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
