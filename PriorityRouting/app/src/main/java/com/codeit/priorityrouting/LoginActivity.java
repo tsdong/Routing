3110819da3bf56ac708de7f2af506caed8116001
James Smith <jnsmith@oakland.edu>
2015-03-04 13:42:53 -0500

    No Summary

--- PriorityRouting/app/src/main/java/com/codeit/priorityrouting/LoginActivity.java
+++ PriorityRouting/app/src/main/java/com/codeit/priorityrouting/LoginActivity.java
@@ -31,10 +31,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends ActionBarActivity {
 
     @Override
@@ -59,6 +55,5 @@ public class LoginActivity extends ActionBarActivity {
                 startActivity(i);
             }
         });
-
     }
 }
