1f32c255ad87d9a675146e5e0b56e033b7b20439
HaoWeng <hao.weng@onstar.com>
2015-03-22 00:01:09 -0400

    add verification to handle when email, password and confirmPassword are null

---
+++ PriorityRouting/app/src/main/java/com/codeit/priorityrouting/DBSupport/Insert/LogInObject.java
@@ -0,0 +1,30 @@
+package com.codeit.priorityrouting.DBSupport.Insert;
+
+/**
+ * Created by FZDDFL on 3/19/2015.
+ */
+public class LogInObject {
+    public String userName;
+    public String password;
+
+
+   /* private List<Map<String,String>> legalDocList;
+
+    public List<Map<String,String>> getDocuments(){return legalDocList;}
+
+    public String getEmail() {
+        return this.userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setEmail(String mail) {
+        this.userName=mail;
+    }
+
+    public void setPassword(String pass) {
+        this.password=pass;
+    }*/
+}
